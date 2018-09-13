bc3cd5e718e70f1327a9ddbcba725a996ee2c5d1
aThorp96 <andrew.thorp@protonmial.com>
2018-09-13 10:12:33 -0400

    BUILDS: connects to google api, invalid scope

--- requests.go
+++ requests.go
@@ -3,57 +3,57 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
 
 func MakeRequest(speedTest SpeedTest) {
-
-	ctx := context.Background()
-
-	c := getClient(ctx)
-
-	sheetsService, err := sheets.New(c)
-	spreadsheetId := "1C7ZgtjrDqrnJ0jYD7iZTdf0i5dpdgmg_fwVvMhpTQx0"
-
-	req := sheets.AppendCellsRequest{
-		SheetId: 1,
-		Rows:    [4]string{speedTest.Time, speedTest.Up, speedTest.Down, speedtest.Server.Name},
-		Fields:  "*"} // List of updates to apply
-
-	dataRange := "A:D"
-	valueInputOption := "USER_ENTERED"
-	insertDataOption := "INSERT_ROWS"
-
-	vr := sheetsService.Spreadsheets.ValueRange
-	vr.Range = "A:D"
-	vr.MajorDimension = "ROWS"
-
-	up := strconv.FormatFloat(speedTest.Up, 'f', 0, 64)
-	down := strconv.FormatFloat(speedTest.Down, 'f', 0, 64)
-
-	vr.values = [1][4]string{{speedTest.Time, up, down, speedTest.Server.Name}}
-
-	rb := &sheets.ValueRange{}
-
-	resp, err := sheetsService.Spreadsheets.Values.Append(spreadsheetId, dataRange, rb).ValueInputOption(valueInputOption).InsertDataOption(insertDataOption).Context(ctx).Do()
+	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
-	fmt.Printf("%#v\n", resp)
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/sheets.googleapis.com-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, "https:www.googleapis.com/auth/spreadsheets.readwrite")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets Client %v", err)
+	}
+
+	spreadsheetId := "1C7ZgtjrDqrnJ0jYD7iZTdf0i5dpdgmg_fwVvMhpTQx0"
+
+	writeRange := "A1"
+
+	var vr sheets.ValueRange
+
+	myval := []interface{}{speedTest.Time, speedTest.Up, speedTest.Down, speedTest.Server.Name}
+	vr.Values = append(vr.Values, myval)
+
+	_, err = srv.Spreadsheets.Values.Update(spreadsheetId, writeRange, &vr).ValueInputOption("USER_ENTERED").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet. %v", err)
+	}
+
 }
 
-func getClient(ctx context.Context) *http.Client {
+func getClient(config *oauth2.Config) *http.Client {
 	tokFile := "token.json"
 	tok, err := tokenFromFile(tokFile)
 	if err != nil {
+		tok = getTokenFromWeb(config)
 		saveToken(tokFile, tok)
 	}
 	return config.Client(context.Background(), tok)
@@ -69,6 +69,24 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
 	return tok, err
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
 }
 
 // Saves a token to a file path.
