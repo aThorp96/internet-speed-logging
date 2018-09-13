e882026d3790eb3f7b7f8747a4fa185d69052834
aThorp96 <andrew.thorp@protonmial.com>
2018-09-12 22:01:48 -0400

    Unpacks json

--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	//"time"
 )
 
+type SpeedTest struct {
+	Down   float32 `json:"download"`
+	Up     float32 `json:"upload"`
+	Server Server  `json:"server"`
+	Time   string  `json:"timestamp"`
+}
+
+type Server struct {
+	Name string `json:"name"`
+}
+
 func main() {
+
 	cmd := exec.Command("/bin/bash", "run.sh")
 	err := cmd.Run()
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(err)
 	}
+
+	//statePath := "state.json"
+	speedPath := "speed.json"
+	jsonFile, err := os.Open(speedPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var speedTest SpeedTest
+
+	bytes, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(bytes, &speedTest)
+
+	fmt.Printf("Upload speed   : %f\n", speedTest.Up)
+	fmt.Printf("Download speed : %f\n", speedTest.Down)
+	fmt.Println("Server        : " + speedTest.Server.Name)
+	fmt.Println("Timestamp     : " + speedTest.Time)
+
+	defer jsonFile.Close()
 }
